56c3815f5121fd83158f236f9988a1912940866a
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2021-08-11 16:38:56 +0300

    Created app model containing initial info

---
+++ CV/CV/Code/Services/AppModel.swift
@@ -0,0 +1,12 @@
+//
+//  AppModel.swift
+//  CV
+//
+//  Created by Krasimir Stoyanov on 11.08.21.
+//
+
+import Foundation
+
+class AppModel: ObservableObject {
+    var profile: Profile = Profile(name: "Krasimir Stoyanov", role: "Mobile Developer", description: "Passionate about everything mobile", location: "Sofia, Bulgaria", skills: [Skill(id: UUID().uuidString, skillName: "Android", image: "android")], experiences: [Experience(id: UUID().uuidString, companyName: "Mobile Waves Solutions", role: "Android Developer", duration: "Feb 2019-Dec 2020")])
+}
