468bfdb7cb2f4f64a41dc25cb0b0dca2cc1736b0
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2021-08-11 10:42:23 +0300

    New project

---
+++ CV/CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by Krasimir Stoyanov on 11.08.21.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
