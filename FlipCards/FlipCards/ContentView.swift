c2c36cbd2dada2b1b01490b6d2a603d0331d665b
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2021-08-11 10:59:36 +0300

    Added FlipCards project

---
+++ FlipCards/FlipCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlipCards
+//
+//  Created by Krasimir Stoyanov on 14.09.20.
+//  Copyright © 2020 Krasimir Stoyanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
